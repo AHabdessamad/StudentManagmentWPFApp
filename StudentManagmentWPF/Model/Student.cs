a250f69f949cff4bf348e33e9c624243288f4691
medLmtToInfinity <med.lmt012@gmail.com>
2023-12-27 13:09:33 +0100

    Model, View, ViewModel
    Adding Models, views and ViewModel and some utilies that helps us in databinding...

---
+++ StudentManagmentWPF/Model/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagmentWPF.Model
+{
+    enum Gender
+    {
+        Homme,
+        Femme
+    }
+    class Student
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public Gender Sexe { get; set; }
+        public DateOnly DateNaissance { get; set; }
+        [Required]
+        public string Cne { get; set; }
+        [Column(TypeName = "LongBlob")]
+        public byte[]? Image { get; set; }
+
+    }
+}
---
+++ StudentManagmentWPF/Model/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagmentWPF.Model
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public Gender Sexe { get; set; }
+        public DateOnly DateNaissance { get; set; }
+        public bool IsAdmin { get; set; }
+        [Column(TypeName = "LongBlob")]
+        public byte[]? Image { get; set; }
+    }
+}
